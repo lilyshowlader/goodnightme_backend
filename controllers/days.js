9b3845a84cafb686ff11b8dd7d381f9765213370
lilyshowlader <lilyshowlader@gmail.com>
2022-11-23 21:29:43 -0500

    import/mount days router

---
+++ controllers/days.js
@@ -0,0 +1,4 @@
+import {Profile} from '../models/profile.js'
+import { Day } from '../models/day.js'
+
+export {}
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import formData from 'express-form-data'
 
 import { router as profilesRouter } from './routes/profiles.js'
 import { router as authRouter } from './routes/auth.js'
-
+import { router as daysRouter } from './routes/days.js'
 import './config/database.js'
 
 const app = express()
@@ -18,6 +18,7 @@ app.use(formData.parse())
 
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/days', daysRouter)
 
 app.use(function (req, res, next) {
   res.status(404).json({ err: 'Not found' })
